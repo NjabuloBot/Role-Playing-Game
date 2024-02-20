2f44bad197ed9181e570fda80e3bfb17d5d97f0b
Njabulo <nnshiwu@gmail.com>
2024-02-20 13:50:48 +0200

    updated the goldText element to display the new value of gold, and updated the text element to display You now have a new weapon..

--- script.js
+++ script.js
@@ -95,7 +95,9 @@ function buyHealth(){
 function buyWeapon(){
    if(gold >= 30){
       gold-=30;
-      currentWeapon+=1;
+      currentWeapon++;
+      goldText.innerText = gold;
+      text.innerText = "You now have a new weapon.";
    }
 }
 
