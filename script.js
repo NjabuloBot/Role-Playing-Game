5c474d972c6360b4d6c3b8ba8fe7929d13535a95
Njabulo <nnshiwu@gmail.com>
2024-02-20 08:32:27 +0200

    Created another property object called button functions. Gave this property an array containing the three functions assigned to the onclick properties in the goTown function.

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let inventory = ["stick"];
 const locations = [
    {
    name:"town square",
-   "button text": ["Go to store","Go to cave","Fight dragon"]
+   "button text": ["Go to store","Go to cave","Fight dragon"],
+   "button functions":[goStore,goCave,fightDragon]
    }
 ];
 
