8b62cd1b3b8342b29ac39e9da45e1eb6187539dc
Njabulo <nnshiwu@gmail.com>
2024-02-22 15:22:21 +0200

    updated goldText and xpText to display the updated values.

--- script.js
+++ script.js
@@ -191,6 +191,8 @@ function dodge(){
 function defeatMonster(){
    gold+=Math.floor(monsters[fighting].level * 6.7);
    xp+=monsters[fighting].level;
+   goldText.innerText = gold;
+   xpText.innerText = xp;
 }
 
 function lose(){
