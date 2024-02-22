000e0c4ef4f35a91053d1890d959223e4187f8c6
Njabulo <nnshiwu@gmail.com>
2024-02-22 14:08:00 +0200

    Displayed the monsterStats element by updating the display property of the style property to block.

--- script.js
+++ script.js
@@ -162,6 +162,7 @@ function sellWeapon(){
 function goFight(){
    update(locations[3]);
    monsterHealth = monsters[fighting].health;
+   monsterStats.style.display = "block";
 }
 
 function attack(){
