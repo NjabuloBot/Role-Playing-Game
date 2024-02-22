3148cddcb99617bfef12fdc4b49dabf101486ced
Njabulo <nnshiwu@gmail.com>
2024-02-22 09:23:03 +0200

    Wrapped all of the code in buyWeapon function inside another if statement. The condition is  checking if currentWeapon is less than 3 - the index of the last weapon.

--- script.js
+++ script.js
@@ -93,17 +93,20 @@ function buyHealth(){
 }
 
 function buyWeapon(){
-   if(gold >= 30){
-      gold-=30;
-      currentWeapon++;
-      goldText.innerText = gold;
-      let newWeapon = weapons[currentWeapon].name;
-      text.innerText = "You now have a "+newWeapon+".";
-      inventory.push(newWeapon);
-      text.innerText+= " In your inventory you have: "+inventory[0]+","+inventory[1];
-   }else{
-      text.innerText = "You do not have enough gold to buy a weapon."
+   if (currentWeapon < 3){
+      if(gold >= 30){
+         gold-=30;
+         currentWeapon++;
+         goldText.innerText = gold;
+         let newWeapon = weapons[currentWeapon].name;
+         text.innerText = "You now have a "+newWeapon+".";
+         inventory.push(newWeapon);
+         text.innerText+= " In your inventory you have: "+inventory[0]+","+inventory[1];
+      }else{
+         text.innerText = "You do not have enough gold to buy a weapon."
+      }
    }
+   
 }
 
 function fightSlime(){
